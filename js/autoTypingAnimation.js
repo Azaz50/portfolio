4c5582c54016ed742cec07c6f88366e68fe0ce74
Azaz Mohammad <azazmohammad14@gmail.com>
2025-01-16 23:25:29 +0530

    Adding some information

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Azaz Mohammad</span> I am a skilled MERN stack developer with a strong background in building dynamic and responsive web applications. I specialize in using MongoDB, Express.js, React.js, and Node.js to create full-stack applications. My expertise includes front-end development with React, back-end development with Node.js and Express, and database management with MongoDB.<br/><br/>I have done 6 month of Internship as a Full Stack Developer in Appzillians PVT LTD.I am passionate about developing efficient and scalable solutions, and I enjoy tackling challenging problems in web development. "
+        "<span>Hi, I am Azaz Mohammad</span> I am a skilled Full Stack developer with a strong background in building dynamic and responsive web applications. I specialize in using Vue.js for front-end development and PHP Laravel for back-end development. My expertise includes crafting user-friendly interfaces, developing robust server-side logic, and managing databases to deliver efficient and scalable web applications.<br/><br/>I have done 6 month of Internship as a Full Stack Developer in Appzillians PVT LTD.I am passionate about developing efficient and scalable solutions, and I enjoy tackling challenging problems in web development. "
 
       ],
       typeSpeed: 7,
