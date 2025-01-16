af833e7c4d3b36b215a3efc5ab20159c99ea0d69
Azaz Mohammad <azazmohammad14@gmail.com>
2025-01-16 23:44:35 +0530

    modify my portfolio

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Azaz Mohammad</span> I am a skilled Full Stack developer with a strong background in building dynamic and responsive web applications. I specialize in using Vue.js for front-end development and PHP Laravel for back-end development. My expertise includes crafting user-friendly interfaces, developing robust server-side logic, and managing databases to deliver efficient and scalable web applications.<br/><br/>I have done 6 month of Internship as a Full Stack Developer in Appzillians PVT LTD.I am passionate about developing efficient and scalable solutions, and I enjoy tackling challenging problems in web development. "
+        "<span>Hi, I am Azaz Mohammad</span> I am a skilled Full Stack developer with a strong background in building dynamic and responsive web applications. I specialize in using Vue.js for front-end development and PHP Laravel for back-end development. My expertise includes crafting user-friendly interfaces, developing robust server-side logic, and managing databases to deliver efficient and scalable web applications.<br/><br/>Completed a 6-month internship as a Full Stack Developer at Appzillians PVT LTD, gaining hands-on experience in building scalable solutions. Currently working as a Full Stack Developer specializing in Vue.js for the frontend and PHP Laravel for the backend. Successfully delivered the Tripo Project, a ride and delivery platform, and two other projects, focusing on dynamic routing, secure APIs, and responsive, user-friendly interfaces. Passionate about solving complex web development challenges. "
 
       ],
       typeSpeed: 7,
